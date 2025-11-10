b41165a797773e641e40a9a5b678e1308a528c9b
IsmaelFlores23 <ia_quiotof@unicah.edu>
2025-11-10 15:36:58 -0500

    ultima actulizacion

--- src/Pages/Tiempo.jsx
+++ src/Pages/Tiempo.jsx
@@ -4,7 +4,7 @@ import { Tiempo } from "../components/Tiempo";
 export default function Tiempopages() {
     return (
         <>
-        <h1>Mi Web Principal</h1>
+       
         <Tiempo></Tiempo>
         </>
     )
